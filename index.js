55c3e5cbe8be4caac13f48e1a0986f9f9fe76545
VICTORIA JOANA KRIGER CONTI <49439065@est.ort.edu.ar>
2026-03-16 12:03:43 -0300

    faltas el six seven

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { sumar, restar, multiplicar, dividir, areaCirculo, pi } from "./src/modules/matematica.js";
 import Alumno from "./src/models/alumno.js";
+import fs from 'fs';
+import url from 'url';
 
 console.log("EJERCICIO 1");
-const texto1= "sofi";
-const texto2= "vicky";
+const texto1 = "sofi";
+const texto2 = "vicky";
 console.log(texto1);
 console.log(texto2);
 console.log(texto1 + " y " + texto2 + " BFFS 4EVER");
@@ -24,3 +26,38 @@ console.log("Username: @" + Sofi.username);
 console.log("DNI: " + Sofi.DNI);
 console.log("Username: @" + Valu.username);
 console.log("DNI: " + Valu.DNI);
+console.log("\n\n");
+
+console.log("EJERCICIO 4");
+function copiar() {
+    const origen = "./entrada.txt";
+    const destino = "./salida.txt";
+    fs.readFile(origen, 'utf-8', (err, contenido) => {
+        if (err) {
+            console.log("Error: Asegúrate de que 'entrada.txt' exista.");
+            return;
+        }
+        fs.writeFile(destino, contenido, (err) => {
+            if (err) {
+                console.error("Error al escribir:", err);
+                return;
+            }
+            console.log('El archivo fue copiado exitosamente');
+        });
+    });
+}
+copiar();
+console.log("\n\n");
+
+console.log("EJERCICIO 5");
+function parsearUrl(direccion) {
+    const miUrl = new URL(direccion);
+    const params = Object.fromEntries(miUrl.searchParams);
+    return {
+        "host": `${miUrl.protocol}//${miUrl.host}`,
+        "pathname": miUrl.pathname,
+        "parametros": params
+    };
+}
+let objeto = parsearUrl("https://campus.ort.edu.ar/secundaria/almagro/informatica/tp/2342788/tp-2-primera-practica-en-node");
+console.log(objeto);
